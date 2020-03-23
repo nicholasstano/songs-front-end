93e623a60b8cf188c53493c65abc3e15861d6def
nicholasstano <nicholasstano@gmail.com>
2020-03-23 10:24:24 -0400

    Removed Todos for now

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React, { Component } from 'react'
 import SongsContainer from '../src/containers/SongsContainer'
-import TodosContainer from '../src/containers/TodosContainer'
 import { Provider } from 'react-redux'
 import store from './store'
 
@@ -11,7 +10,6 @@ export class App extends Component {
     return (
       <Provider store={store}>
         <div>
-          <TodosContainer />
           <SongsContainer />
         </div>
       </Provider>
